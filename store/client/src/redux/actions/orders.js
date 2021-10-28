a7476d0d9752aea34bc4b252a6a58dd359bd5909
Mohammed <80047190+mohammed884@users.noreply.github.com>
2021-10-29 01:04:45 +0300

    Delete store directory

--- store/client/src/redux/actions/orders.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { types } from "./types";
-
-export const getOrders = (query) => async dispatch => {
-    const url = `${process.env.REACT_APP_SERVER_URL}/admin/orders?finished=${query.finished}&canceled=${query.canceled}`;
-    const res = await axios(url, {withCredentials:true});
-    dispatch({type:types.ORDERS_LOADED, payload:res.data});
-}   
