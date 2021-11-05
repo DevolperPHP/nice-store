3d02080d265b490fbf66c738fa47c88a62875fef
Mohammed <80047190+mohammed884@users.noreply.github.com>
2021-11-05 03:04:31 +0300

    Delete store directory

--- store/api/routes/getIndividualData.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-const Product = require('../modules/products');
-
-const getIndividualData = async (number, cb) => {
-    const month = moment().subtract(number, 'months').format('M/YYYY');
-    const products = await Product.find({analysisDate:{$gte:month}, buyScore:{$gt:0}});
-    return products;
-}
-module.exports = getIndividualData;
