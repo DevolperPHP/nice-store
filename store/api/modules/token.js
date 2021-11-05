fff949561b147deefa88e7bf26c2acc1a88843c2
Mohammed <80047190+mohammed884@users.noreply.github.com>
2021-11-06 01:13:53 +0300

    Delete store directory

--- store/api/modules/token.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = new mongoose.Schema({
-    token:{
-        type:String,
-        required:true,
-    },
-});
-const Token = new mongoose.model('token', Schema);
-module.exports = Token
