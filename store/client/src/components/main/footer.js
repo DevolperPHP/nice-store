b0ce1db5a02b8677d4856cdb543ea4fdf11cf976
Mohammed <80047190+mohammed884@users.noreply.github.com>
2021-11-08 00:59:05 +0300

    Delete store directory

--- store/client/src/components/main/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../../style/main/footer.css'
-export default function Footer() {
-    return (
-        <footer>
-            <p>High End Store by Mohammed Instagram @1kdd1</p>
-        </footer>
-    )
-}
