efdddd69cc89e6cd724b175476f789872f226e83
Mohammed <80047190+mohammed884@users.noreply.github.com>
2021-11-09 11:37:53 +0300

    Update delete_account.js

--- client/src/components/account/delete_account.js
+++ client/src/components/account/delete_account.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { Redirect, useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import '../../style/account/verify_account.css';
 export default function DeleteAccount({ userReducer }) {
     const [password, setPassword] = useState('');
     const [errMsg, setErrMsg] = useState(false);
-    const {token} = useParams()
+    const {token} = useParams();
+    const history = useHistory();
     const handleDelete = async (e) => {
         e.preventDefault()
         const url = `${process.env.REACT_APP_SERVER_URL}/confirm/delete/${token}`;
         const res = await axios.post(url, { password }, { withCredentials: true });
         if (!res.data.done) setErrMsg(res.data.errMsg);
-        else <Redirect to="/" />
+        else history.push('/');
     }
     return (
-        <section className="delete-account-section">
+        <section className="verify-account-section">
             <form onSubmit={handleDelete}>
                 {errMsg}
                 <label htmlFor="">
-                    <p>Write Your Password to confirm deleting</p>
+                    <p>Enter Password to confirm deleting</p>
                     <input type="text" onChange={e => setPassword(e.target.value)} />
+                    <button type="submit">Delete</button>
                 </label>
-                <button type="submit">Delete</button>
+                
             </form>
         </section>
     )
