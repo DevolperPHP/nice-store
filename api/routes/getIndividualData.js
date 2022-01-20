cf2f4a2aeb98768701203e0a5011c47f5fe1a806
Mohammed Abdulaziz <80047190+mohammed884@users.noreply.github.com>
2022-01-20 23:45:42 +0300

    Update getIndividualData.js

--- api/routes/getIndividualData.js
+++ api/routes/getIndividualData.js
@@ -4,21 +4,10 @@ const Product = require("../modules/products");
 const getIndividualData = async (number) => {
   const monthsArr =
     number === "current-month" ? [moment().format("M/YYYY")] : [];
-  for (let i = 1; i <= number; i++) {
-    monthsArr.push(moment().subtract(i, "months").format("M/YYYY"));
-  }
-
-  const products = [];
+  for (let i = 1; i <= number; i++) monthsArr.push(moment().subtract(i, "months").format("M/YYYY"));
+  const products = await Product.find({analysisDate: { $in: monthsArr },buyScore: { $gt: 0 }});
   let totalSales = 0;
-  for (let x = 0; x < monthsArr.length; x++) {
-    const currentProducts = await Product.find({
-      analysisDate: { $eq: monthsArr[x] },
-      buyScore: { $gt: 0 },
-    });
-    for (let y = 0; y < currentProducts.length; y++)
-      totalSales += currentProducts[y].buyScore;
-    products.push(...currentProducts);
-  }
+  for (let y = 0; y < currentProducts.length; y++) totalSales += currentProducts[y].buyScore;
   return { products, totalSales };
 };
 module.exports = getIndividualData;
